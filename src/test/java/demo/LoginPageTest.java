cfc9750fccafde69bf15206e8d455e618e43c9fa
luisf <luisf@DESKTOP-P0D3UQL>
2023-05-08 17:45:45 -0700

    updading

--- src/test/java/demo/LoginPageTest.java
+++ src/test/java/demo/LoginPageTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginPageTest {
@@ -48,5 +47,4 @@ public class LoginPageTest {
 		Thread.sleep(3000);
 		driver.quit();
 	}
-
 }
